9cf7dc6d1d5fc7983996765cbcf4b5c4dbd0f38c
Дутченко Олег <dutchenko.o.wezom@gmail.com>
2018-08-04 14:37:10 +0300

    start

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Browserizr</title>
+</head>
+<body>
+
+</body>
+</html>
